a5f8e75c96d97f572f1d838538ec69ec24fa3221
jcesar <jovansonlee.cesar@truste.com>
2015-11-27 21:42:43 +0800

    clean separation of string conversion and the api

--- src/lib.rs
+++ src/lib.rs
@@ -5,28 +5,43 @@ use std::ffi::CString;
 use std::str;
 use libc::c_char;
 
+static mut initialized: bool = false;
+
 #[no_mangle]
 pub extern "C" fn get_data(arg1: *const c_char, arg2: *const c_char) -> *const c_char {
+    
+    unsafe {if !initialized{
+        init();
+        initialized = true;
+    }}
+
     let s1 = unsafe { CStr::from_ptr(arg1) };
     let s2 = unsafe { CStr::from_ptr(arg2) };
 	
 	let str1 = s1.to_str().unwrap();
 	let str2 = s2.to_str().unwrap();
-	println!("str1: {}", str1);
-	println!("str2: {}", str2);
+    
+    let content = retrieve_data(str1, str2);
+
+	let ret = CString::new(content.as_bytes()).unwrap();
+	ret.into_raw()
+}
+
+fn retrieve_data(arg1: &str, arg2: &str)->String{
+ 	println!("arg1: {}", arg1);
+	println!("arg2: {}", arg2);
 
 	let content = format!(r#"
 	{{
 		"message": "Hello,
 		"arg1": {},
 		"arg2": {}, 
-	}}"#, str1, str2);
+	}}"#, arg1, arg2);
 
-	println!("content: \n{}", content);
-	let ret = CString::new(content.as_bytes()).unwrap();
-	ret.into_raw()
+	println!("content: \n{}", content); 
+    content
 }
 
-fn rust_substrings(value: &str, substr: &str) -> i32 {
-    value.matches(substr).count() as i32
+fn init(){
+    println!("Initializing....")
 }
